ade3dd43aa76fd63af73f3bc910bc65f5b7ce7b8
Alexa <gametopsss@gmail.com>
2023-08-24 08:37:49 +0300

    headers fix

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ headers = {
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
     'Connection': 'keep-alive',
-    # 'Cookie': 'PHPSESSID=nbpahmh62v964avqnmb8gp1gb7; _ym_uid=1692850126258532426; _ym_d=1692850126; _ym_isad=1; _ym_visorc=w',
     'Referer': 'http://sevtransport-online.ru/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest',
